c68ac64fc834d661b6f4f1322931bd0548208272
yapisango <yapisango@gmail.com>
2025-04-02 13:33:41 +0200

    Updated Vans app
    Added some functionality

--- api.js
+++ api.js
@@ -1,4 +1,30 @@
-export async function getVans(id) {
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4InWJZ3br-O_QhA2q9tPCDny1LMNrgxQ",
+  authDomain: "vanlife-1ee5d.firebaseapp.com",
+  projectId: "vanlife-1ee5d",
+  storageBucket: "vanlife-1ee5d.firebasestorage.app",
+  messagingSenderId: "612348439860",
+  appId: "1:612348439860:web:7abc9d1380c06e11b78961"
+};
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+const vansCollectionRef = collection(db, "vans")
+
+export async function getVans() {
+    const snapshot = await getDocs(vansCollectionRef)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
+
+/**export async function getVans(id) {
     const url = id ? `/api/vans/${id}` : "/api/vans"
     const res = await fetch(url)
     if (!res.ok) {
@@ -10,7 +36,7 @@ export async function getVans(id) {
     }
     const data = await res.json()
     return data.vans
-}
+}*/
 
 export async function getHostVans(id) {
     const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
